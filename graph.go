2e812c96f372804af33af2f9d6de5232ccacf3ab
dSamp41 <d.sampietro@studenti.unipi.it>
2025-07-06 15:15:10 +0200

    Create graph.go

---
+++ graph.go
@@ -0,0 +1,43 @@
+package main
+
+import "sync"
+
+type Node struct {
+	label 		string
+	input 		chan string
+	successors 	[]*Node
+}
+
+func NewNode(s string) *Node {
+	return &Node{
+		label: s,
+		input: make(chan string, 100), 	//add buffer to make async
+		successors: []*Node{},
+	}
+}
+
+
+func (n *Node) GenerateSenetence(wg *sync.WaitGroup, resultCh chan<- string) {
+	go func(){
+		for msg := range n.input {
+			newMsg := msg + " " + n.label
+			
+			if len(n.successors) == 0 {	//terminal node
+				resultCh <- newMsg
+				wg.Done()
+			} else {
+				for _, succ := range n.successors {
+					wg.Add(1)
+					succ.input <- newMsg
+				}
+				wg.Done()
+			}
+		}
+	}()
+}
+
+
+type Graph map[int]*Node
+
+func (g Graph) AddNode() {}
+func (g Graph) AddEdge() {}

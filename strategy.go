8a3b896aed4baaf93bf30198ced15e31567ac3b2
dSamp41 <d.sampietro@studenti.unipi.it>
2025-09-04 16:17:33 +0200

    Remove comments

--- strategy.go
+++ strategy.go
@@ -8,17 +8,12 @@ import (
 )
 
 func SeqStrategy(graph Graph, max_depth int, resultCh *chann.Chann[Message]) {
-	//SENTENCE GENERATION
-	//resultCh := chann.New[Message]()//make(chan Message, 1000)
-
 	// Setup channel with initial value DFS from each node
 	for _, node := range graph.nodes {
 		if node.label == "." { continue }
 		
 		msg := Message {
-			//sentence: fmt.Sprintf("[FROM %s]", node.label),
 			sentence: []string{fmt.Sprintf("[FROM %s]", node.label)},
-			//visited: map[string]int{node.label: 1},
 			depth: 0,
 		}
 		//node.input.In() <- msg // Start traversal with empty message
@@ -26,9 +21,7 @@ func SeqStrategy(graph Graph, max_depth int, resultCh *chann.Chann[Message]) {
 		node.GenerateSentenceSeq(msg, resultCh.In(), max_depth)
 	}
 
-
 	// Wait for all paths to finish: since it is seq, we wait for all GenerateSentenceSeq to finish
-	//resultCh.Close()
 }
 
 
@@ -44,9 +37,7 @@ func ParStrategy(graph Graph, max_depth int, resultCh *chann.Chann[Message]){
 		wg.Add(1)
 		
 		msg := Message {
-			//sentence: fmt.Sprintf("[FROM %s]", node.label),
 			sentence: []string{fmt.Sprintf("[FROM %s]", node.label)},
-			//visited: map[string]int{node.label: 1},
 			depth: 0,
 		}
 		node.input.In() <- msg // Start traversal with empty message
